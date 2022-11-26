2e328bf68e526fab62e3fa21d775d83ebf28c91b
artem_balykov <artem.balikov@yandex.ru>
2022-11-26 15:31:00 +0700

    created function find_user

--- database/db_maneger.py
+++ database/db_maneger.py
@@ -25,5 +25,11 @@ def push_user(id, name, des, tg, has_team, skills, SKILLS):
         cur.execute("INSERT INTO user_skill VALUES(?, ?);", user_skill)
     base.commit()
 
+def find_user(skills):
+    global cur, base
+    cur.execute("SELECT * FROM (USER INNER JOIN user_skill ON USER.id=user_skill.id_user) INNER JOIN SKILLS ON SKILLS.id=user_skill.id_skill;")
+    print(str(cur.fetchall()))
 SKILLS = db_start()
-push_user(12643, "Vlad", "cool gay", "@Vladik", True, ['C++', 'Kotlin', 'PHP'], SKILLS)
+#push_user(12643, "Vlad", "cool gay", "@Vladik", True, ['C++', 'Kotlin', 'PHP'], SKILLS)
+
+find_user(['C++'])
